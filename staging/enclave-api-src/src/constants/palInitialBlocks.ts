b1b190bb5c1f5d960bf4d0d70685228441c6c3f9
Hinkal CI <ci@hinkal.pro>
2026-07-30 19:25:42 +0000

    deploy: staging 4e9555ccd090b89d973303c42548c143e4a24f25

--- staging/enclave-api-src/src/constants/palInitialBlocks.ts
+++ staging/enclave-api-src/src/constants/palInitialBlocks.ts
@@ -1,6 +1,7 @@
 import { chainIds } from '@hinkal/common';
 
 export const PAL_EVENTS_INITIAL_BLOCK_BY_CHAIN: Partial<Record<number, number>> = {
+  [chainIds.bnbMainnet]: 112845135,
   [chainIds.ethMainnet]: 25080500,
   [chainIds.polygon]: 86781000,
   [chainIds.arbMainnet]: 462119000,
